9f969c9901d1074445331527a39307cd74f7d92d
Witsaroot <pccphet77@gmail.com>
2023-04-25 02:11:04 +0700

    First draft which is complete basic register

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
+
+//Route files
+const auth = require('./routes/auth.js');
+
+//Load env vars
+dotenv.config({path:'./config/config.env'});
+
+//Connect to database
+connectDB();
+
+const app = express();
+
+//Body parser
+app.use(express.json());
+
+//Cookie parser
+app.use(cookieParser());
+
+//Mount routers
+app.use('/api/Online-Job-Fair/auth', auth);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+});
